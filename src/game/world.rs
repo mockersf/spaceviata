cb1d942cad784d29a4a139b8ff46211929c7f8cb
François <mockersf@gmail.com>
2022-11-15 01:11:34 +0100

    control camera with mouse or keyboard

--- src/game/world.rs
+++ src/game/world.rs
@@ -1,4 +1,10 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use std::time::Duration;
+
+use bevy::{
+    input::mouse::{MouseMotion, MouseWheel},
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
 
 use crate::{assets::GalaxyAssets, game::World, GameState};
 
@@ -16,7 +22,9 @@ impl bevy::app::Plugin for Plugin {
             .add_system_set(
                 SystemSet::on_update(CURRENT_STATE)
                     .with_system(update_camera)
-                    .with_system(update_camera_controller),
+                    .with_system(update_camera_controller)
+                    .with_system(camera_keyboard_controls)
+                    .with_system(camera_mouse_controls),
             )
             .add_system_set(SystemSet::on_exit(CURRENT_STATE).with_system(tear_down));
     }
@@ -109,13 +117,72 @@ fn update_camera_controller(
     target: Res<CameraControllerTarget>,
     time: Res<Time>,
 ) {
-    let speed = 8.0;
+    let speed = 5.0;
     if controller.position.distance_squared(target.position) > 15.0 {
         let towards = (target.position - controller.position) * time.delta_seconds() * speed;
         controller.position += towards;
     }
-    if (controller.zoom_level - target.zoom_level).abs() > 0.05 {
+    if (controller.zoom_level - target.zoom_level).abs() > 0.01 {
         let towards = (target.zoom_level - controller.zoom_level) * time.delta_seconds() * speed;
         controller.zoom_level += towards;
+    }
+}
+
+fn camera_keyboard_controls(
+    controller: Res<CameraController>,
+    mut target: ResMut<CameraControllerTarget>,
+    keyboard_input: Res<Input<KeyCode>>,
+) {
+    let mut order = Vec2::ZERO;
+    if keyboard_input.any_pressed([KeyCode::Right, KeyCode::D]) {
+        order.x += 1.0;
+    }
+    if keyboard_input.any_pressed([KeyCode::Left, KeyCode::A]) {
+        order.x -= 1.0;
+    }
+    if keyboard_input.any_pressed([KeyCode::Up, KeyCode::W]) {
+        order.y += 1.0;
+    }
+    if keyboard_input.any_pressed([KeyCode::Down, KeyCode::S]) {
+        order.y -= 1.0;
+    }
+    if order != Vec2::ZERO {
+        let order = order.normalize();
+        target.position = controller.position + order * (controller.zoom_level / 4.0 + 10.0);
+    }
+    if keyboard_input.just_pressed(KeyCode::PageUp) {
+        target.zoom_level = (controller.zoom_level + 1.0).min(10.0);
+    }
+    if keyboard_input.just_pressed(KeyCode::PageDown) {
+        target.zoom_level = (controller.zoom_level - 1.0).max(1.0);
+    }
+}
+
+fn camera_mouse_controls(
+    controller: Res<CameraController>,
+    mut target: ResMut<CameraControllerTarget>,
+    mouse_input: Res<Input<MouseButton>>,
+    mut mouse_motion: EventReader<MouseMotion>,
+    mut mouse_wheel: EventReader<MouseWheel>,
+    mut pressed_at: Local<Option<Duration>>,
+    time: Res<Time>,
+) {
+    if mouse_input.just_pressed(MouseButton::Left) {
+        *pressed_at = Some(time.raw_elapsed())
+    }
+    if mouse_input.just_released(MouseButton::Left) {
+        *pressed_at = None;
+    }
+    if let Some(when) = *pressed_at {
+        if (time.raw_elapsed() - when).as_secs_f32() > 0.2 {
+            for motion in mouse_motion.iter() {
+                target.position = controller.position
+                    + (motion.delta * Vec2::new(-1.0, 1.0)) * (40.0 / controller.zoom_level);
+            }
+        }
+    }
+    mouse_motion.clear();
+    for wheel in mouse_wheel.iter() {
+        target.zoom_level = (controller.zoom_level - wheel.y).clamp(1.0, 10.0);
     }
 }
