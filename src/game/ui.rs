12fc1c16f766b94453f0a7243a7afdfaa76387e8
François <mockersf@gmail.com>
2022-11-21 23:57:20 +0100

    some scope

--- src/game/ui.rs
+++ src/game/ui.rs
@@ -86,183 +86,189 @@ fn setup(
 ) {
     info!("loading UI");
 
-    let button_handle = ui_handles.button_handle.clone_weak();
-    let button = buttons.get(&button_handle).unwrap();
-    let material = ui_handles.font_material.clone_weak();
+    // menu
+    {
+        let button_handle = ui_handles.button_handle.clone_weak();
+        let button = buttons.get(&button_handle).unwrap();
+        let material = ui_handles.font_material.clone_weak();
 
-    let zoom_in_button = button.add(
-        &mut commands,
-        Val::Px(40.),
-        Val::Px(40.),
-        UiRect::all(Val::Auto),
-        material.clone(),
-        UiButtons::ZoomIn,
-        25.,
-        crate::ui_helper::ColorScheme::TEXT,
-    );
-    let zoom_out_button = button.add(
-        &mut commands,
-        Val::Px(40.),
-        Val::Px(40.),
-        UiRect::all(Val::Auto),
-        material.clone(),
-        UiButtons::ZoomOut,
-        25.,
-        crate::ui_helper::ColorScheme::TEXT,
-    );
-    let game_menu_button = button.add(
-        &mut commands,
-        Val::Px(40.),
-        Val::Px(40.),
-        UiRect::all(Val::Auto),
-        material,
-        UiButtons::GameMenu,
-        25.,
-        crate::ui_helper::ColorScheme::TEXT,
-    );
-    let back_to_menu_button = button.add(
-        &mut commands,
-        Val::Px(100.),
-        Val::Px(40.),
-        UiRect::all(Val::Auto),
-        ui_handles.font_sub.clone_weak(),
-        UiButtons::BackToMenu,
-        25.,
-        crate::ui_helper::ColorScheme::TEXT,
-    );
+        let zoom_in_button = button.add(
+            &mut commands,
+            Val::Px(40.),
+            Val::Px(40.),
+            UiRect::all(Val::Auto),
+            material.clone(),
+            UiButtons::ZoomIn,
+            25.,
+            crate::ui_helper::ColorScheme::TEXT,
+        );
+        let zoom_out_button = button.add(
+            &mut commands,
+            Val::Px(40.),
+            Val::Px(40.),
+            UiRect::all(Val::Auto),
+            material.clone(),
+            UiButtons::ZoomOut,
+            25.,
+            crate::ui_helper::ColorScheme::TEXT,
+        );
+        let game_menu_button = button.add(
+            &mut commands,
+            Val::Px(40.),
+            Val::Px(40.),
+            UiRect::all(Val::Auto),
+            material,
+            UiButtons::GameMenu,
+            25.,
+            crate::ui_helper::ColorScheme::TEXT,
+        );
+        let back_to_menu_button = button.add(
+            &mut commands,
+            Val::Px(100.),
+            Val::Px(40.),
+            UiRect::all(Val::Auto),
+            ui_handles.font_sub.clone_weak(),
+            UiButtons::BackToMenu,
+            25.,
+            crate::ui_helper::ColorScheme::TEXT,
+        );
 
-    commands
-        .spawn((
-            NodeBundle {
-                style: Style {
-                    size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
-                    ..default()
-                },
-                ..default()
-            },
-            ScreenTag,
-        ))
-        .with_children(|commands| {
-            commands
-                .spawn(NodeBundle {
+        commands
+            .spawn((
+                NodeBundle {
                     style: Style {
-                        position: UiRect {
-                            right: Val::Px(20.0),
-                            top: Val::Px(20.0),
-                            ..default()
-                        },
-                        size: Size {
-                            width: Val::Px(100.0),
-                            height: Val::Px(150.0),
-                        },
-                        flex_direction: FlexDirection::Column,
-                        justify_content: JustifyContent::SpaceAround,
-                        position_type: PositionType::Absolute,
+                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
                         ..default()
                     },
                     ..default()
-                })
-                .with_children(|builder| {
-                    builder
-                        .spawn(NodeBundle {
-                            style: Style {
-                                flex_direction: FlexDirection::Row,
-                                justify_content: JustifyContent::SpaceAround,
+                },
+                ScreenTag,
+            ))
+            .with_children(|commands| {
+                commands
+                    .spawn(NodeBundle {
+                        style: Style {
+                            position: UiRect {
+                                right: Val::Px(20.0),
+                                top: Val::Px(20.0),
                                 ..default()
                             },
+                            size: Size {
+                                width: Val::Px(100.0),
+                                height: Val::Px(150.0),
+                            },
+                            flex_direction: FlexDirection::Column,
+                            justify_content: JustifyContent::SpaceAround,
+                            position_type: PositionType::Absolute,
                             ..default()
-                        })
-                        .push_children(&[zoom_in_button, zoom_out_button]);
-                })
-                .push_children(&[game_menu_button, back_to_menu_button])
-                .with_children(|builder| {
-                    builder
-                        .spawn((
-                            NodeBundle {
+                        },
+                        ..default()
+                    })
+                    .with_children(|builder| {
+                        builder
+                            .spawn(NodeBundle {
                                 style: Style {
-                                    flex_direction: FlexDirection::Column,
+                                    flex_direction: FlexDirection::Row,
                                     justify_content: JustifyContent::SpaceAround,
                                     ..default()
                                 },
-                                visibility: Visibility::INVISIBLE,
                                 ..default()
-                            },
-                            MenuContainer,
-                        ))
-                        .push_children(&[back_to_menu_button]);
-                });
-        });
+                            })
+                            .push_children(&[zoom_in_button, zoom_out_button]);
+                    })
+                    .push_children(&[game_menu_button, back_to_menu_button])
+                    .with_children(|builder| {
+                        builder
+                            .spawn((
+                                NodeBundle {
+                                    style: Style {
+                                        flex_direction: FlexDirection::Column,
+                                        justify_content: JustifyContent::SpaceAround,
+                                        ..default()
+                                    },
+                                    visibility: Visibility::INVISIBLE,
+                                    ..default()
+                                },
+                                MenuContainer,
+                            ))
+                            .push_children(&[back_to_menu_button]);
+                    });
+            });
+    }
 
-    let base = commands
-        .spawn((
-            NodeBundle {
-                style: Style {
-                    flex_direction: FlexDirection::Column,
-                    margin: UiRect::all(Val::Px(10.0)),
-                    size: Size {
-                        width: Val::Percent(100.0),
-                        height: Val::Percent(100.0),
-                    },
-                    overflow: Overflow::Hidden,
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            ScreenTag,
-        ))
-        .with_children(|parent| {
-            parent.spawn((
+    // left panel
+    {
+        let base = commands
+            .spawn((
                 NodeBundle {
                     style: Style {
                         flex_direction: FlexDirection::Column,
-                        flex_grow: 1.0,
-                        max_size: Size::UNDEFINED,
+                        margin: UiRect::all(Val::Px(10.0)),
+                        size: Size {
+                            width: Val::Percent(100.0),
+                            height: Val::Percent(100.0),
+                        },
+                        overflow: Overflow::Hidden,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                ScreenTag,
+            ))
+            .with_children(|parent| {
+                parent.spawn((
+                    NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            flex_grow: 1.0,
+                            max_size: Size::UNDEFINED,
+                            ..default()
+                        },
                         ..default()
                     },
-                    ..default()
-                },
-                StarList::default(),
-            ));
-        })
-        .id();
+                    StarList::default(),
+                ));
+            })
+            .id();
 
-    let panel_style = Style {
-        position_type: PositionType::Absolute,
-        position: UiRect {
-            left: Val::Px(0.),
-            right: Val::Undefined,
-            bottom: Val::Undefined,
-            top: Val::Undefined,
-        },
-        margin: UiRect::all(Val::Px(0.)),
-        justify_content: JustifyContent::Center,
-        align_items: AlignItems::Center,
-        size: Size::new(Val::Px(LEFT_PANEL_WIDTH), Val::Percent(100.0)),
-        align_content: AlignContent::Stretch,
-        flex_direction: FlexDirection::Column,
-        ..Default::default()
-    };
+        let panel_style = Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                left: Val::Px(0.),
+                right: Val::Undefined,
+                bottom: Val::Undefined,
+                top: Val::Undefined,
+            },
+            margin: UiRect::all(Val::Px(0.)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            size: Size::new(Val::Px(LEFT_PANEL_WIDTH), Val::Percent(100.0)),
+            align_content: AlignContent::Stretch,
+            flex_direction: FlexDirection::Column,
+            ..Default::default()
+        };
 
-    commands.spawn((
-        NodeBundle {
-            style: panel_style.clone(),
-            background_color: BackgroundColor(Color::rgba(0.5, 0.5, 0.5, 0.75)),
-            ..default()
-        },
-        ScreenTag,
-    ));
-    commands.spawn((
-        bevy_ninepatch::NinePatchBundle {
-            style: panel_style,
-            nine_patch_data: bevy_ninepatch::NinePatchData::with_single_content(
-                ui_handles.left_panel_handle.1.clone_weak(),
-                ui_handles.left_panel_handle.0.clone_weak(),
-                base,
-            ),
-            ..default()
-        },
-        ScreenTag,
-    ));
+        commands.spawn((
+            NodeBundle {
+                style: panel_style.clone(),
+                background_color: BackgroundColor(Color::rgba(0.5, 0.5, 0.5, 0.75)),
+                ..default()
+            },
+            ScreenTag,
+        ));
+        commands.spawn((
+            bevy_ninepatch::NinePatchBundle {
+                style: panel_style,
+                nine_patch_data: bevy_ninepatch::NinePatchData::with_single_content(
+                    ui_handles.left_panel_handle.1.clone_weak(),
+                    ui_handles.left_panel_handle.0.clone_weak(),
+                    base,
+                ),
+                ..default()
+            },
+            ScreenTag,
+        ));
+    }
 }
 
 #[derive(Component, Default)]
