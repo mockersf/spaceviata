65958e8fc1979e0ec9c08348eb2e694446c4e604
François <mockersf@gmail.com>
2022-11-25 02:31:11 +0100

    don't extract resources on turn 0

--- src/game/turns.rs
+++ src/game/turns.rs
@@ -27,53 +27,55 @@ impl bevy::app::Plugin for Plugin {
 }
 
 fn start_player_turn(mut universe: ResMut<Universe>, mut turns: ResMut<Turns>) {
-    let good_conditions = &universe.galaxy[universe.players[0].start].clone();
+    if turns.count != 0 {
+        let good_conditions = &universe.galaxy[universe.players[0].start].clone();
 
-    universe.players[0].savings += universe.player_revenue(0);
-    let mut harvested = 0.0;
-    universe
-        .galaxy
-        .clone()
-        .iter()
-        .zip(universe.star_details.iter_mut())
-        .filter(|(_, details)| details.owner == 0)
-        .for_each(|(star, details)| {
-            // grow population
-            {
-                let max_population = if star.color == good_conditions.color {
-                    120.0 + (turns.count - details.owned_since) as f32 / 5.0
-                } else {
-                    10.0 + (turns.count - details.owned_since) as f32 / 10.0
-                };
-                let lerp = details.population / max_population;
-                let growth_factor = if lerp < 0.5 {
-                    4.0 * lerp.powf(3.0)
-                } else if lerp < 1.0 {
-                    1.0 - (-2.0 * lerp + 2.0).powf(3.0) / 2.0
-                } else {
-                    1.0 - (-2.0 * lerp + 4.0).powf(3.0) / 2.0
-                };
-                details.population = if star.size == good_conditions.size {
-                    details.population + growth_factor
-                } else {
-                    details.population + growth_factor / 10.0
-                };
-            }
-
-            // harvest resources
-            {
-                let current_resources = (details.resources * 1.2).powf(1.5);
-                let collect = 1.0_f32.min(current_resources);
-                harvested += collect;
-                details.resources = if star.color != good_conditions.color {
-                    ((details.resources * 1.2).powf(1.5) - collect).powf(1.0 / 1.5) / 1.2
-                } else {
-                    ((details.resources).powf(0.8) - collect).powf(1.0 / 0.8)
+        universe.players[0].savings += universe.player_revenue(0);
+        let mut harvested = 0.0;
+        universe
+            .galaxy
+            .clone()
+            .iter()
+            .zip(universe.star_details.iter_mut())
+            .filter(|(_, details)| details.owner == 0)
+            .for_each(|(star, details)| {
+                // grow population
+                {
+                    let max_population = if star.color == good_conditions.color {
+                        120.0 + (turns.count as i32 - details.owned_since) as f32 / 5.0
+                    } else {
+                        10.0 + (turns.count as i32 - details.owned_since) as f32 / 10.0
+                    };
+                    let lerp = details.population / max_population;
+                    let growth_factor = if lerp < 0.5 {
+                        4.0 * lerp.powf(3.0)
+                    } else if lerp < 1.0 {
+                        1.0 - (-2.0 * lerp + 2.0).powf(3.0) / 2.0
+                    } else {
+                        1.0 - (-2.0 * lerp + 4.0).powf(3.0) / 2.0
+                    };
+                    details.population = if star.size == good_conditions.size {
+                        details.population + growth_factor
+                    } else {
+                        details.population + growth_factor / 10.0
+                    };
                 }
-                .max(0.0);
-            }
-        });
-    universe.players[0].resources += harvested;
+
+                // harvest resources
+                {
+                    let current_resources = (details.resources * 1.2).powf(1.5);
+                    let collect = 1.0_f32.min(current_resources);
+                    harvested += collect;
+                    details.resources = if star.color != good_conditions.color {
+                        ((details.resources * 1.2).powf(1.5) - collect).powf(1.0 / 1.5) / 1.2
+                    } else {
+                        ((details.resources).powf(0.8) - collect).powf(1.0 / 0.8)
+                    }
+                    .max(0.0);
+                }
+            });
+        universe.players[0].resources += harvested;
+    }
 
     turns.count += 1;
     turns.messages = vec![format!("turn {}", turns.count)];
