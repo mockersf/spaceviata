a1160e2f8b2dacb4b22af37baa94f79ed777a16f
François <mockersf@gmail.com>
2022-11-30 22:24:03 +0100

    Revert "randomize which player you play against"
    This reverts commit d6bd464b036b48b4517008ff96a9273dc23f3d92.

--- src/game/setup.rs
+++ src/game/setup.rs
@@ -11,7 +11,7 @@ use bevy::{
     },
     sprite::MaterialMesh2dBundle,
 };
-use rand::{seq::SliceRandom, Rng};
+use rand::Rng;
 
 use crate::{
     assets::{names::Names, GalaxyAssets, UiAssets},
@@ -653,17 +653,10 @@ fn tear_down(
     let mut fleets = vec![];
 
     let seed = rand.gen_range(0..creator.nb_players) as usize;
-    let mut bots = [1, 2, 3, 4];
-    bots.shuffle(&mut rand);
-    let mut players = vec![0];
-    players.append(&mut bots.to_vec());
-
-    let players = players
+    let players = (0..(creator.nb_players as usize))
         .into_iter()
-        .enumerate()
-        .take(creator.nb_players as usize)
-        .map(|(i, player)| {
-            let mut angle = PI * 2.0 / creator.nb_players as f32 * (i + seed) as f32;
+        .map(|player| {
+            let mut angle = PI * 2.0 / creator.nb_players as f32 * (player + seed) as f32;
             if creator.nb_players % 2 == 0 {
                 angle += PI / (creator.nb_players as f32 * 1.5);
             }
